37bb525dadb83a902cb29670a57b3f77805f42ce
StanPlatinum <lpjlwjlpjlwj@126.com>
2020-03-16 19:43:46 -0400

    tsx in sgx worked

--- rtmsgx_test/Enclave/Enclave.cpp
+++ rtmsgx_test/Enclave/Enclave.cpp
@@ -18,6 +18,7 @@ void rtm_test(){
 	int status;
 	int nonce = 0;
 	int mutex = 0;
+	int xtest_status = 0;
 
 	PrintDebugInfo("--- test 1 ---\n");
 	if ((status = _xbegin()) == _XBEGIN_STARTED) {
@@ -26,7 +27,7 @@ void rtm_test(){
 	} else {
 		PrintDebugInfo("trans failed...\n");
 	}
-	PrintDebugInfo("aborted status %x, # %d\n", status, _XABORT_CODE(status));
+	PrintDebugInfo("trans finished.\n");
 
 	PrintDebugInfo("--- test 2 ---\n");
 	if ((status = _xbegin()) == _XBEGIN_STARTED) {
@@ -42,9 +43,10 @@ void rtm_test(){
 	PrintDebugInfo("--- test 3 ---\n");
 	if ((status = _xbegin()) == _XBEGIN_STARTED) {
 		if (_xtest()) {
+			xtest_status = _xtest();
 		}
 		_xend();
 	} else
 		PrintDebugInfo("aborted status %x, # %d\n", status, _XABORT_CODE(status));
-
+	PrintDebugInfo("testing if in a trans..., xtest_status: %d\n", xtest_status);
 }
